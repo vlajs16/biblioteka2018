8fdffa819f25e9b3e758d3b5b1edd0a39b4e4f95
vlajs16 <vladimir.belca@gmail.com>
2018-03-09 10:41:20 +0100

    Implementirana klasa Biblioteka

---
+++ Biblioteka/src/biblioteka/Biblioteka.java
@@ -0,0 +1,38 @@
+package biblioteka;
+
+import java.util.LinkedList;
+
+import biblioteka.interfejs.BibliotekaInterfejs;
+
+public class Biblioteka implements BibliotekaInterfejs {
+	
+	private LinkedList<Knjiga> knjige = new LinkedList<Knjiga>();
+	
+
+	@Override
+	public void dodajKnjigu(Knjiga knjiga) {
+		knjige.add(knjiga);
+	}
+
+	@Override
+	public void obrisiKnjigu(Knjiga knjiga) {
+		knjige.remove(knjiga);
+	}
+
+	@Override
+	public LinkedList<Knjiga> vratiSveKnjige() {		
+		return knjige;
+	}
+
+	@Override
+	public LinkedList<Knjiga> pronadjiKnjigu(String naslov, Autor autor, String isbn, String izdavac) {
+		LinkedList<Knjiga> novaLista = new LinkedList<Knjiga>();
+		
+		for(int i = 0; i<knjige.size(); i++) 
+			if(knjige.get(i).getNaslov().contains(naslov))
+				novaLista.add(knjige.get(i));
+		
+		return novaLista;
+	}
+
+}
